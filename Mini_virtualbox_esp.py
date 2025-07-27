98ec8b6a31aec5c292276b2ebbbf8ffd84607c30
Comfrey29 <tiopepa37@gmail.com>
2025-07-27 09:26:41 +0200

    Apple_Old_VM_Launcher_esp.py

--- Mini_virtualbox_esp.py
+++ Mini_virtualbox_esp.py
@@ -1,4 +1,4 @@
-﻿#! /usr/bin/env python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
@@ -7,97 +7,97 @@ os.system(cmd)
 import sys
 import subprocess
 
-def executar_comanda(comanda):
+def ejecutar_comando(comando):
     try:
-        proc = subprocess.Popen(comanda, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = proc.communicate()
         return proc.returncode, stdout, stderr
     except Exception, e:
-        return -1, '', 'Exception: ' + str(e)
+        return -1, '', 'Excepción: ' + str(e)
 
-def comprovar_vboxmanage():
-    codi, _, _ = executar_comanda(['which', 'VBoxManage'])
-    if codi != 0:
-        print "Error: VBoxManage no està disponible al PATH."
+def comprobar_vboxmanage():
+    codigo, _, _ = ejecutar_comando(['which', 'VBoxManage'])
+    if codigo != 0:
+        print "Error: VBoxManage no está disponible en el PATH."
         sys.exit(1)
 
-def existeix_vm(nom_vm):
-    codi, sortida, err = executar_comanda(['VBoxManage', 'list', 'vms'])
-    if codi != 0:
-        print "Error llistant màquines virtuals:", err
+def existe_vm(nombre_vm):
+    codigo, salida, err = ejecutar_comando(['VBoxManage', 'list', 'vms'])
+    if codigo != 0:
+        print "Error listando máquinas virtuales:", err
         sys.exit(1)
-    return nom_vm in sortida
+    return nombre_vm in salida
 
-def crear_vm(nom_vm):
-    print "Creant VM:", nom_vm
-    codi, _, err = executar_comanda(['VBoxManage', 'createvm', '--name', nom_vm, '--register'])
-    if codi != 0:
-        print "Error creant VM:", err
-        sys.exit(1)
-
-    codi, _, err = executar_comanda(['VBoxManage', 'modifyvm', nom_vm, '--memory', '1024', '--acpi', 'on', '--boot1', 'dvd'])
-    if codi != 0:
-        print "Error modificant VM:", err
+def crear_vm(nombre_vm):
+    print "Creando VM:", nombre_vm
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'createvm', '--name', nombre_vm, '--register'])
+    if codigo != 0:
+        print "Error creando VM:", err
         sys.exit(1)
 
-    codi, _, err = executar_comanda(['VBoxManage', 'storagectl', nom_vm, '--name', 'IDE Controller', '--add', 'ide'])
-    if codi != 0:
-        print "Error creant controladora IDE:", err
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'modifyvm', nombre_vm, '--memory', '1024', '--acpi', 'on', '--boot1', 'dvd'])
+    if codigo != 0:
+        print "Error modificando VM:", err
         sys.exit(1)
 
-    codi, _, err = executar_comanda(['VBoxManage', 'storageattach', nom_vm,
-                                    '--storagectl', 'IDE Controller',
-                                    '--port', '0', '--device', '0',
-                                    '--type', 'dvddrive', '--medium', 'emptydrive'])
-    if codi != 0:
-        print "Error assignant drive DVD buit:", err
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'storagectl', nombre_vm, '--name', 'IDE Controller', '--add', 'ide'])
+    if codigo != 0:
+        print "Error creando controlador IDE:", err
         sys.exit(1)
 
-def assignar_iso(nom_vm, ruta_iso):
-    print "Assignant ISO:", ruta_iso
-    codi, _, err = executar_comanda(['VBoxManage', 'storageattach', nom_vm,
-                                     '--storagectl', 'IDE Controller',
-                                     '--port', '0', '--device', '0',
-                                     '--type', 'dvddrive', '--medium', ruta_iso])
-    if codi != 0:
-        print "Error assignant ISO:", err
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'storageattach', nombre_vm,
+                                       '--storagectl', 'IDE Controller',
+                                       '--port', '0', '--device', '0',
+                                       '--type', 'dvddrive', '--medium', 'emptydrive'])
+    if codigo != 0:
+        print "Error asignando unidad DVD vacía:", err
         sys.exit(1)
 
-def iniciar_vm(nom_vm):
-    print "Iniciant VM:", nom_vm
-    codi, _, err = executar_comanda(['VBoxManage', 'startvm', nom_vm, '--type', 'gui'])
-    if codi != 0:
-        print "Error iniciant VM:", err
+def asignar_iso(nombre_vm, ruta_iso):
+    print "Asignando ISO:", ruta_iso
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'storageattach', nombre_vm,
+                                       '--storagectl', 'IDE Controller',
+                                       '--port', '0', '--device', '0',
+                                       '--type', 'dvddrive', '--medium', ruta_iso])
+    if codigo != 0:
+        print "Error asignando ISO:", err
         sys.exit(1)
 
-def ajuda():
+def iniciar_vm(nombre_vm):
+    print "Iniciando VM:", nombre_vm
+    codigo, _, err = ejecutar_comando(['VBoxManage', 'startvm', nombre_vm, '--type', 'gui'])
+    if codigo != 0:
+        print "Error iniciando VM:", err
+        sys.exit(1)
+
+def ayuda():
     print """
-Ús: %s <Nom_VM> <Ruta_a_ISO>
+Uso: %s <Nombre_VM> <Ruta_a_ISO>
 
-Aquest script crea (si no existeix), assigna la ISO i inicia la VM.
+Este script crea (si no existe), asigna la ISO e inicia la VM.
 """ % sys.argv[0]
 
 def main():
     if len(sys.argv) != 3:
-        ajuda()
+        ayuda()
         sys.exit(1)
 
-    comprovar_vboxmanage()
+    comprobar_vboxmanage()
 
-    nom_vm = sys.argv[1]
+    nombre_vm = sys.argv[1]
     ruta_iso = sys.argv[2]
 
     if not os.path.isfile(ruta_iso):
-        print "Error: La ISO indicada no existeix:", ruta_iso
+        print "Error: La ISO indicada no existe:", ruta_iso
         sys.exit(1)
 
-    if not existeix_vm(nom_vm):
-        crear_vm(nom_vm)
+    if not existe_vm(nombre_vm):
+        crear_vm(nombre_vm)
     else:
-        print "La VM '%s' ja existeix. Reutilitzant-la." % nom_vm
+        print "La VM '%s' ya existe. Reutilizándola." % nombre_vm
 
-    assignar_iso(nom_vm, ruta_iso)
-    iniciar_vm(nom_vm)
+    asignar_iso(nombre_vm, ruta_iso)
+    iniciar_vm(nombre_vm)
 
 if __name__ == '__main__':
     main()
